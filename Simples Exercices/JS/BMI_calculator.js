1989525d9712a3cec1260732e983573a920c47fd
Youry Tamisier <youry@dizgual.com.br>
2025-04-28 13:20:51 -0300

    updating simples functions with ifelse statment

--- Simples Exercices/JS/BMI_calculator.js
+++ Simples Exercices/JS/BMI_calculator.js
@@ -10,7 +10,17 @@ bmi should equal 20 when it's rounded to the nearest whole number.
 */
 function calculateBMI(weight, height) {
 
-    return Math.floor( weight/(height**2))
+    var bmi = weight/(height * height);
+    
+    if (bmi < 18.5) {
+        return console.log("Your BMI is " + bmi +", so you are underweight");
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+        return console.log("Your BMI is " + bmi +", so you have a normal weight");
+    } else {
+        return console.log("Your BMI is " + bmi +", so you are overweight");
+    }
+      
+
 }
 
 console.log(calculateBMI(65,1.8))
---
+++ Simples Exercices/JS/loveCalculator.js
@@ -0,0 +1,6 @@
+var myName = prompt("What's your name?");
+var loveName = prompt("What's your loves name?")
+
+var percentageOfLove = Math.random() * 100;
+percentageOfLove = Math.floor(percentageOfLove) + 1;
+console.log(percentageOfLove + "%");
