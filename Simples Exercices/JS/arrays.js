539b16fee59a92b9a1e4928b9f2d8691fe4cafdc
sm5963 <youry.tamisier@external.engie.com>
2025-06-02 13:44:27 -0300

    array simples com loope for

---
+++ Simples Exercices/JS/arrays.js
@@ -0,0 +1,31 @@
+// nomeDoArray.includes(variavel) - Vai verificar se o valor existe no array
+//.push() - adiciona itens em um array. Sempre coloca no fim do array
+//.pop - retira o ultimo número do array
+
+var messageOutput = [];
+var numeroInicial = 1;
+var fizzOrBuzz =["Fizz", "Buzz", "FizzBuzz"]
+
+function confereFizzBuzz (numero) {
+  if((numero % 3 == 0) && (numero % 5 == 0)){
+    return fizzOrBuzz[2];
+  }
+  if(numero % 3 == 0) {
+    return fizzOrBuzz[0];
+  }
+  if(numero % 5 == 0) {
+    return fizzOrBuzz[1];
+  }
+  return numero;
+}
+
+function printFizzBuzz() {
+  let completaArray = confereFizzBuzz(numeroInicial);
+  messageOutput.push(completaArray);
+  numeroInicial += 1;
+  console.log(messageOutput);
+}
+
+for (let i=0; i<=100; i++){
+  printFizzBuzz();
+}
